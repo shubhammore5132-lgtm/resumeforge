33b4484325d26d46724b9c9807ccf5372c4daf4a
shubhammore5132-lgtm <shubhammore5132@gmail.com>
2026-03-16 20:48:31 +0530

    fix railway deploy structure

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/resumeDB");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected");
   } catch (error) {
-    console.error("Database connection failed:", error);
+    console.error(error);
     process.exit(1);
   }
 };
